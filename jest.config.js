365c0f0327eab4b7724d0081c50350645423019d
DA19s <digital@africanity-group.com>
2025-12-18 14:11:53 +0000

    test

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.js'],
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/server.js',
+    '!src/config/**',
+  ],
+  coverageDirectory: 'coverage',
+  verbose: true,
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
+};
+
+
+
+
+
